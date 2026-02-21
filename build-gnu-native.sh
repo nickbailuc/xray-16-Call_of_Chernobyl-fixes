9ed475b7bec20fde103d8e9f0b599a963784a50d
nickbailuc <35620734+nickbailuc@users.noreply.github.com>
2026-02-20 23:15:52 -0500

    Create build-gnu-native.sh

---
+++ build-gnu-native.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir bin
+cd bin
+
+# Use optimization for current CPU architecture
+CFLAGS="-march=native"
+CXXFLAGS="-march=native"
+
+# Use GNU C Library malloc instead of Microsoft mimalloc
+cmake -DMEMORY_ALLOCATOR=standard ..
+
+# Confirm & compile
+printf "\nPress [Enter] to compile or [Ctrl+C] to cancel > "; read && \
+make -j$(nproc)
